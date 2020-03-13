003b2a242c2c82eee0aec753d3763e3626306692
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-03-13 03:08:12 +0200

    Heroku DB connection string

--- Cloud_Tema2_Node/config.js
+++ Cloud_Tema2_Node/config.js
@@ -1,1 +1,1 @@
-module.exports.MONGO_CONNECTION_STRING = 'mongodb://localhost:27017/Cloud_Tema2';
+module.exports.MONGO_CONNECTION_STRING = 'mongodb://admin:admin1337@ds263656.mlab.com:63656/heroku_nj33fz8p';
